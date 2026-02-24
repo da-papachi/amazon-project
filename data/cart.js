7f9c37f8fce4d3f91461181bfcc3c126a8c3ed18
papachi <amirumurahunov60@gmail.com>
2026-02-24 15:05:06 +0500

    Generate HTML for order-summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,11 @@
-export const cart = [];
+export const cart = [{
+ productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+ quantity: 2
+},
+{
+  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+ quantity: 1
+}];
 
 export function addToCart(button) {
 
@@ -14,10 +21,7 @@ export function addToCart(button) {
 
         cart.push({
           productId: button.dataset.productId,
-          productName: button.dataset.productName,
-          productImage: button.dataset.productImage,
-          productPrice: button.dataset.productPrice,
-          
+
           quantity: Number(document.querySelector(`.js-quantity-selector-${button.dataset.productId}`).value)
         });
         
