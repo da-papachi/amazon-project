338487639557e54f70ec5c02288f71c838534225
papachi <amirumurahunov60@gmail.com>
2026-02-25 23:02:48 +0500

    delete products from cart when click delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,13 @@
-import {cart} from "../data/cart.js";
+import {cart, removeFromCart} from "../data/cart.js";
 import {products} from '../data/products.js';
 import { formatCurrency } from "./utils/money.js";
 
 
 const cartGrid = document.querySelector(".js-order-summary");
-cart.forEach((cartItem) => {
+
+function createCartList() {
+  cartGrid.innerHTML = ``
+    cart.forEach((cartItem) => {
 
     const productId = cartItem.productId
 
@@ -45,7 +48,8 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link"
+                  data-product-id=${matchingProduct.id}>
                     Delete
                   </span>
                 </div>
@@ -99,4 +103,16 @@ cart.forEach((cartItem) => {
           </div>
     `
 
+})
+}
+
+
+createCartList()
+
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener('click', () => {
+    removeFromCart(link.dataset.productId)
+    console.log(cart)
+    createCartList()
+  })
 })
